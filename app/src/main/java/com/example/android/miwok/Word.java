e48a9ea633c16f9435a57be6a0974329201a2bca
Marvin Martinez <martinez.webdev@gmail.com>
2017-12-31 05:08:27 -0500

    Successfully connected each list item with correct audio file. Updated Word class to accept audio resource. Implemented resource release for activity life cycle onStop() for each activity.

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -5,18 +5,21 @@ public class Word {
 
     private String mDefaultTranslation;
     private String mMiwokTranslation;
+    private int mAudioResourceId;
     private int mImageResourceId = NO_IMAGE_PROVIDED;
     private static final int NO_IMAGE_PROVIDED = -1;
 
-    public Word(String mDefaultTranslation, String mMiwokTranslation) {
+    public Word(String mDefaultTranslation, String mMiwokTranslation, int mAudioResourceId) {
         this.mDefaultTranslation = mDefaultTranslation;
         this.mMiwokTranslation = mMiwokTranslation;
+        this.mAudioResourceId = mAudioResourceId;
     }
 
-    public Word(String mDefaultTranslation, String mMiwokTranslation, int mImageResourceId) {
+    public Word(String mDefaultTranslation, String mMiwokTranslation, int mImageResourceId, int mAudioResourceId) {
         this.mDefaultTranslation = mDefaultTranslation;
         this.mMiwokTranslation = mMiwokTranslation;
         this.mImageResourceId = mImageResourceId;
+        this.mAudioResourceId = mAudioResourceId;
     }
 
     public String getmDefaultTranslation() {
@@ -28,6 +31,8 @@ public class Word {
     }
 
     public int getmImageResourceId() { return mImageResourceId; }
+
+    public int getmAudioResourceId() { return mAudioResourceId; }
 
     public boolean hasImage() {
         return mImageResourceId != NO_IMAGE_PROVIDED;
